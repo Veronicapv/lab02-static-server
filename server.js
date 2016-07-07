9146dca45d0a751e1819a72092a0ad7fe3037893
Veronica <pikacha16hotmail.com>
2016-07-07 08:51:23 -0500

    Lab IP-PORT Server con ip y puerto tomados del entorno

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //http
 var http = require("http");
+var PORT = process.env.PORT || 3000;
+var IP = process.env.IP || '127.0.0.1'
 // crear un servidor e basico 
 var server = http.createServer(function (req,res){
     // aramar la respuesta
@@ -14,6 +16,6 @@ res.write("hola mundo");
 res.end();
 });
 //poner a trabajar al a servidor 
-server.listen(3000,'127.0.0.1',function () {
+server.listen(3000,'192.168.1.35',function () {
     console.log("> Server listening http://localhost:3000...");
 });
