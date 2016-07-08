d13d033e64bce351ac6e1abde62c4ff8a1f974f9
Veronica <pikacha16hotmail.com>
2016-07-08 08:56:02 -0500

    LAB06 CONFILE  SE CARGAN CONFIGURACIONES  DE UN JSON

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 //http
 var http = require("http");
 var fs = require('fs');
-var PORT = process.env.PORT || 3000;
-var IP = process.env.IP || '127.0.0.1';
-if(IP=='127.0.0.1'){
-    console.log(">----EJECUTANDO EN MODO LOCAL----");
+var config = require("./config/config.js");
+//obteniendo las configuraciones
+// del modulo de configuraciones
+var PORT = config.PORT;
+var IP = config.IP;
+if(IP=='127.0.0.1');{
+    console.log("-------ejecutando en modo local-----");
 }
-// crear un servidor e basico 
+
 var server = http.createServer(function (req,res){
     // aramar la respuesta
     // armar el encabezado
@@ -15,22 +18,17 @@ var server = http.createServer(function (req,res){
         "Server":"ITGAM4.2.4"
     }); 
     //lectura del archivo a servir
-    fs.readFile('./static/index.html','utf8',function(err, content){
+    fs.readFile('./static/index.html',
+    'utf8',function(err, content){
         if(err){
-            res.write("<h1>Error de lectura</h1>");
-            res.end();
+    res.end("<h1>Error de lectura</h1>");
+           
         }else{
-            res.write(content);
-            res.end();
+            res.end(content);
         }
     });
-    
-    // enviamos la respuesta
-res.write("<h1>Luis Cruz</h1>");
-// cerra la conexion
-res.end();
 });
 //poner a trabajar al a servidor 
-server.listen(3000,'192.168.1.35',function () {
-    console.log("> Server listening http://localhost:3000...");
+server.listen(PORT,IP,function () {
+    console.log(`> Server listening http://${IP}:${PORT}...`);
 });
