7965d37a0b738c1b10f84d9ccba00b37e02e91b8
Veronica <pikacha16hotmail.com>
2016-07-13 08:22:11 -0500

    lab o9

--- static/js/site.js
+++ static/js/site.js
@@ -1,16 +1,19 @@
-function hola(){
-    alert("Bienvenido  a esta paguina...");
+function hola() {
+    sweetAlert("Worl lace",
+        "¡Bienvenido a esta paguina!");
+    document.body.style.backgroundColor = "lightseagreen";
+
 }
 
-function changeBkgColor(){
+function changeBkgColor() {
     var color = ducument.body.style.backgroundColor;
     document.get
-    if(color == "lightseagreen"){
-        color="honsydew";
-    }else{
-        color="lightseagreen";
+    if (color == "lightseagreen") {
+        color = "honsydew";
+    } else {
+        color = "lightseagreen";
     }
-    console.log(">Cambiando color " + color );
-element.style.backgroundColor = color;
+    console.log(">Cambiando color " + color);
+    element.style.backgroundColor = color;
 
-    }
+}
